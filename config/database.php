0bedd2d47e3daa48b2e314d0c76be0ad3d9fc2ce
ahmedfathy5012 <44382585+ahmedfathy5012@users.noreply.github.com>
2020-06-09 15:02:37 -0700

    auth

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env(parse_url(getenv("CLEARDB_DATABASE_URL"))),
+            'host' => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_299b4d4f283bd1d'),
+            'username' => env('DB_USERNAME', 'bf43dfce03a0b0'),
+            'password' => env('DB_PASSWORD', '7ded15fe'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
